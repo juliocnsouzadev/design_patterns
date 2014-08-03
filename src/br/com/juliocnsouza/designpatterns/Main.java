f2ef15e6ddffdca91def098e08abbe7bfd9c861f
juliocnsouza <juliocnsouzadev@gmail.com>
2014-08-03 14:49:18 -0300

    firts commit

---
+++ src/br/com/juliocnsouza/designpatterns/Main.java
@@ -0,0 +1,20 @@
+package br.com.juliocnsouza.designpatterns;
+
+
+/**
+ * Main.java -> Job:
+ * @since 03/08/2014
+ * @version 1.0
+ * @author Julio Cesar Nunes de Souza (julio.souza@mobilitasistemas.com.br)
+ */
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
